a051dbb528d1d03142d7326eddaa3306ebe44fd2
sieuq <sieuq@DESKTOP-69Q2UO9>
2017-10-22 02:27:10 +0700

    Add project files.

---
+++ Math is Fun/Source.cpp
@@ -0,0 +1,35 @@
+﻿// variable
+
+using namespace std;
+#include <iostream>
+#include <math.h>
+
+float a, b, tong, hieu, thuong, nhan;
+// math
+	int main()
+{
+
+	cout << "A = ";
+	cin >> a;
+	// b 
+	cout << "B = ";
+	cin >> b;
+
+	// phép cộng +
+	tong = a + b;
+	cout << "Tong la  " << tong << endl; 
+	// phép trừ 
+	hieu = a - b;
+	cout << "Hieu la " << hieu << endl;
+	//phép nhân
+	nhan = a*b;
+	cout << "Ket qua phep nhan la " << nhan << endl;
+
+	// phép chia
+	thuong = a/b;
+	cout << "Ket qua phep chia " << thuong << endl;
+	
+	system("Pause");
+	return 0;
+}
+
---
+++ Ngay 1/1- Hello World.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	cout << "Welcome to 30 Day c++ challenge" << endl;
+	system("Pause");
+}
